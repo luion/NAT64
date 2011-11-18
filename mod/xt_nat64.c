1fee1a8cdd2341e5d07ba2dbe0d3d790b0072933
Luis Hinojosa <lf.hinojosa@gmail.com>
2011-11-18 11:03:55 -0600

    added f_n_u code; 0 compilation issues; implementing it now

--- mod/xt_nat64.c
+++ mod/xt_nat64.c
@@ -68,6 +68,7 @@
 #include "xt_nat64.h"
 #include "nf_nat64_generic_functions.h"
 #include "nf_nat64_auxiliary_functions.h"
+#include "nf_nat64_filtering_and_updating.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Juan Antonio Osorio <jaosorior@gmail.com>");
@@ -221,6 +222,39 @@ static int nat64_send_packet(struct sk_buff * old_skb, struct sk_buff *skb)
 
 	return ret;
 }
+
+/*
+ * BEGIN: NAT64 Filter and updating configuration variables and settings.
+ */
+
+int previousTime, currentTime;
+
+// Configuration variables
+int udp_min = 120;
+int udp_default = 300;
+int tcp_trans = 240;
+int tcp_est = 7200;
+int tcp_incoming_syn = 6;
+int fragment_mint = 2;
+int icmp_default = 60;
+
+int udp_period;
+struct nat64_bib *udp_bib;
+struct nat64_st *udp_st;
+
+/*
+ * This structure's purpose is getting the L4 layer respective function to get
+ * the outgoing tuple.
+ */
+struct nat64_outtuple_func {
+	struct nf_conntrack_tuple * (* get_outtuple)(union nf_inet_addr, 
+			u_int16_t, union nf_inet_addr, u_int16_t, 
+			u_int8_t, u_int8_t);
+};
+
+/*
+ * END: NAT64 Filter and updating configuration variables and settings.
+ */
 
 /*
  * Function that gets the pointer directed to it's 
@@ -666,22 +700,22 @@ static bool nat64_determine_outgoing_tuple(u_int8_t l3protocol,
 	 */
 	new_skb = nat64_get_skb(l3protocol, l4protocol, skb, inner);
 
-/*	if (!new_skb) {
+	if (!new_skb) {
 		pr_debug("NAT64: Skb allocation failed -- returned NULL");
 		return false;
 	}
 
-*/	/*
+	/*
 	 * Adjust the layer 3 protocol variable to be used in the outgoing tuple
 	 * Wether it's IPV4 or IPV6 is already checked in the nat64_tg function
 	 */
-/*	l3protocol = (l3protocol == NFPROTO_IPV4) ? NFPROTO_IPV6 : NFPROTO_IPV4;
-*/
+	l3protocol = (l3protocol == NFPROTO_IPV4) ? NFPROTO_IPV6 : NFPROTO_IPV4;
+
 	/*
 	 * Adjust the layer 4 protocol variable to be used 
 	 * in the outgoing tuple.
 	 */
-/*	if (l4protocol == IPPROTO_ICMP) {
+	if (l4protocol == IPPROTO_ICMP) {
 		l4protocol = IPPROTO_ICMPV6;
 	} else if (l4protocol == IPPROTO_ICMPV6) {
 		l4protocol = IPPROTO_ICMP;
@@ -696,7 +730,7 @@ static bool nat64_determine_outgoing_tuple(u_int8_t l3protocol,
 	}
 	
 	pr_debug("NAT64: Determining the outgoing tuple stage went OK.");
-*/	
+	
 	/*
 	 * TODO: Implement call to translate_packet to get the new packet
 	 * from the tuple.
@@ -711,7 +745,174 @@ static bool nat64_determine_outgoing_tuple(u_int8_t l3protocol,
 static bool nat64_update_n_filter(u_int8_t l3protocol, u_int8_t l4protocol, 
 		struct sk_buff *skb, struct nf_conntrack_tuple * inner)
 {
-	return true;
+
+	struct nat64_bib_entry *bib_entry;
+	struct nat64_st_entry *st_entry;
+	struct nat64_ipv4_ta *ipv4_pool_ta;
+	struct nat64_ipv6_ta *ipv6_ta;
+	bool res;
+	
+	res = false;
+	if (l3protocol == NFPROTO_IPV4) {
+		if (inner->dst.protonum == NFPROTO_IPV4) {
+			/*
+			* Dropping packet
+			*/
+			return res;
+		} else if (inner->dst.protonum == NFPROTO_IPV6) {
+			// if the dst address is IPv6, query the STs for any records
+			// if there's no active session for the specified connection,
+			// the packet should be dropped
+			switch (l4protocol) {
+				case IPPROTO_TCP:
+					//Query TCP ST
+					pr_debug("NAT64: TCP protocol not currently supported.");
+				break;
+				case IPPROTO_UDP:
+					//Query UDP ST
+					if (true) {
+						//continue processing...
+						//FIXME
+					} else {
+						pr_debug("NAT64: no currently active session; packet dropped.");
+                    			}
+				break;
+				case IPPROTO_ICMP:
+					//Query ICMP ST
+					pr_debug("NAT64: ICMP protocol not currently supported.");
+				break;
+				case IPPROTO_ICMPV6:
+					//Query ICMPV6 ST
+					pr_debug("NAT64: ICMPv6 protocol not currently supported.");
+				break;
+				default:
+					//Drop packet
+					pr_debug("NAT64: layer 4 protocol not currently supported.");
+					break;
+				}
+		}
+	} else if (l3protocol == NFPROTO_IPV6) {
+		if (inner->dst.protonum == NFPROTO_IPV6) {
+			/*
+			*	Testing...
+			*/
+			//FIXME: DROP? ACCEPT? This is hairpinning.
+			return res;
+	        } else if (inner->dst.protonum == NFPROTO_IPV4) {
+			switch (l4protocol) {
+				case IPPROTO_TCP:
+					/*
+					* Verify if there's any binding for the src address by querying
+					* the TCP BIB. If there's a binding, verify if there's a
+					* connection to the specified destination by querying the TCP ST.
+					* 
+					* In case any of these records are missing, they should be created.
+					*/
+					pr_debug("NAT64: TCP protocol not currently supported.");
+				break;
+				case IPPROTO_UDP:
+					/*
+					* Verify if there's any binding for the src address by querying
+					* the UDP BIB. If there's a binding, verify if there's a
+					* connection to the specified destination by querying the UDP ST.
+					* 
+					* In case these records are missing, they should be created.
+					*/
+//	FIXME: CLOCKS no hay		currentTime = (int) clock() / CLOCKS_PER_SECOND;
+					if (currentTime - previousTime > udp_period) {
+						nat64_st_delete(udp_st, udp_default, currentTime);
+						//Deletes any record whose "lifetime" has exceeded "udp_default"
+						previousTime = currentTime;
+					}
+					//Querying the UDP BIB
+					bib_entry = nat64_bib_select(udp_bib, &(inner->src.u3.in6),
+					inner->src.u.udp.port);
+
+					if (bib_entry == NULL) {
+						//Allocate memory
+						ipv6_ta = (struct nat64_ipv6_ta *)
+						kmalloc(sizeof(struct nat64_ipv6_ta), GFP_KERNEL);
+						if (ipv6_ta != NULL) {
+							//Initialize IPv6 t.a. structure
+							nat64_initialize_ipv6_ta(ipv6_ta, &(inner->src.u3.in6), inner->src.u.udp.port);
+							//Verify if there's an address available in the IPv4 pool
+							ipv4_pool_ta = nat64_ipv4_pool_address_available(ipv6_ta);	
+							if (ipv4_pool_ta != NULL) {
+								//Allocate memory for BIB entry
+								bib_entry = (struct nat64_bib_entry *) kmalloc(sizeof(struct nat64_bib_entry), GFP_KERNEL);
+								//Allocate memory for ST entry
+								st_entry = (struct nat64_st_entry *) kmalloc(sizeof(struct nat64_st_entry), GFP_KERNEL);
+								if (bib_entry != NULL && st_entry != NULL) {
+									//Initialize BIB entry
+									nat64_initialize_bib_entry(bib_entry, 
+										&(inner->src.u3.in6), 
+										inner->src.u.udp.port, 
+										&(ipv4_pool_ta->ip4a), 
+										ipv4_pool_ta->port);
+									//Insert entry into UDP BIB
+									nat64_bib_insert(udp_bib, bib_entry);
+									//Initialize ST entry
+									nat64_initialize_st_entry(st_entry,
+										&(inner->src.u3.in6), inner->src.u.udp.port,
+										&(inner->dst.u3.in6), inner->dst.u.udp.port,
+										&(ipv4_pool_ta->ip4a), ipv4_pool_ta->port,
+										&(inner->dst.u3.in), inner->dst.u.udp.port,
+										currentTime);
+										//Insert entry into UDP ST
+										nat64_st_insert(udp_st, st_entry);
+									res = true;
+								} else {
+									//FIXME: bib_entry = NULL;
+									//FIXME: st_entry = NULL;
+									kfree(bib_entry);
+									kfree(st_entry);
+								}
+							}
+						}
+					} else {
+						//Querying the UDP ST
+						st_entry = nat64_st_select(udp_st, &(bib_entry->ta_4.ip4a),
+							bib_entry->ta_4.port, &(inner->dst.u3.in), inner->dst.u.udp.port);
+						if (st_entry != NULL) {
+							nat64_st_update(udp_st, &(bib_entry->ta_4.ip4a),
+							bib_entry->ta_4.port, &(inner->dst.u3.in),
+							inner->dst.u.udp.port, currentTime);
+							res = true;
+						} else {
+							//Allocate memory for ST entry
+							st_entry = (struct nat64_st_entry *) kmalloc(sizeof(struct nat64_st_entry), GFP_KERNEL);
+							if (st_entry != NULL) {
+								//Initialize ST entry
+								nat64_initialize_st_entry(st_entry,
+									&(inner->src.u3.in6), inner->src.u.udp.port,
+									&(inner->dst.u3.in6), inner->dst.u.udp.port,
+									&(ipv4_pool_ta->ip4a), ipv4_pool_ta->port,
+									&(inner->dst.u3.in), inner->dst.u.udp.port,
+									currentTime);
+								//Insert entry into UDP ST
+								nat64_st_insert(udp_st, st_entry);
+								res = true;
+							}
+						}
+					}
+				break;
+				case IPPROTO_ICMP:
+					//Query ICMP ST
+					pr_debug("NAT64: ICMP protocol not currently supported.");
+				break;
+				case IPPROTO_ICMPV6:
+					//Query ICMPV6 ST
+					pr_debug("NAT64: ICMPv6 protocol not currently supported.");
+				break;
+				default:
+					//Drop packet
+					pr_debug("NAT64: layer 4 protocol not currently supported.");
+				break;
+			}
+		}
+	}
+	pr_debug("NAT64: Updating and Filtering stage went OK.");
+	return res;
 }
 
 /*
@@ -767,15 +968,15 @@ static unsigned int nat64_ipv6_core(struct sk_buff *skb,
 //		nf_ret = nat64_update_n_filter(l3protocol, l4protocol, 
 //			skb, &inner);
 	}
-	pr_debug("ENTRANDO A OUTGOING");
+
 	if(nf_ret) {
 		nf_ret = nat64_determine_outgoing_tuple(l3protocol, l4protocol, 
 			skb, &inner, &new_skb, &outgoing);
 	}
 	
 	if(nf_ret) {
-//		nf_ret = nat64_translate_packet(l3protocol, l4protocol, 
-//			&new_skb, &outgoing);	
+		nf_ret = nat64_translate_packet(l3protocol, l4protocol, 
+			&new_skb, &outgoing);	
 	}
 	
 	/* TODO: Incluir llamada a HAIRPINNING aqui */
